d5111821ae535dcff5916796f07c3eb313d4bd2b
陈明旭 <cloomcmx1554@hotmail.com>
2018-10-20 09:21:27 +0800

    1

---
+++ Materal.DesktopApplication/Materal.WPFCustomControlLib/DateTimePicker.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Materal.WPFCustomControlLib
+{
+    public class DateTimePicker : DatePicker
+    {
+        static DateTimePicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DateTimePicker), new FrameworkPropertyMetadata(typeof(DateTimePicker)));
+        }
+    }
+}
