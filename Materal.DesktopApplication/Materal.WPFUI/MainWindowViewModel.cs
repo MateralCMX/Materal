e0feeadbd23c2a1c2533b9f677e8e1ea136cb056
陈明旭 <cloomcmx1554@hotmail.com>
2018-10-24 13:50:10 +0800

    1

---
+++ Materal.DesktopApplication/Materal.WPFUI/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace Materal.WPFUI
+{
+    public class MainWindowViewModel
+    {
+        private string _title = "Materal";
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title
+        {
+            get => _title + " 当前版本：" + Version;
+            set => _title = value;
+        }
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        private string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+}
