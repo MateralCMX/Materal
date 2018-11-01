5ddca860bd0907c910bf143e4ccc8dca63eb6b92
陈明旭 <cloomcmx1554@hotmail.com>
2018-11-01 18:02:56 +0800

    1、新增-CmdHelper

--- Materal/Materal.ConsoleUI/Program.cs
+++ Materal/Materal.ConsoleUI/Program.cs
@@ -12,24 +12,25 @@ namespace Materal.ConsoleUI
     {
         static void Main(string[] args)
         {
-            //Task.Run(async () => { await Init(); });
-            IEHelper.ClearCache();
+            Task.Run(async () => { await Init(); });
             Console.ReadKey();
         }
 
         public static async Task Init()
         {
-            IMongoDatabase mongoDatabase = new MongoClient("mongodb://220.165.9.44:13398").GetDatabase("BeiDouDB");
-            IMongoCollection<BsonDocument> collection = mongoDatabase.GetCollection<BsonDocument>("DeviceLocation");
-            FilterDefinitionBuilder<BsonDocument> builder = Builders<BsonDocument>.Filter;
-            var minDate = new DateTime(2018, 10, 1, 0, 0, 0);
-            var maxDate = new DateTime(2018, 10, 26, 23, 59, 59);
-            FilterDefinition<BsonDocument> filter = builder.Eq("DeviceID",Guid.Parse("808cbe9f-e08e-4f1d-82b4-07745089d484")) & builder.Gte("CreateTime", minDate) & builder.Lte("CreateTime", maxDate);
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            List<BsonDocument> result = await collection.Find(filter).ToListAsync();
-            stopwatch.Stop();
-            Console.WriteLine($"{result.Count}条数据,耗时:{stopwatch.ElapsedTicks}");
+            string output = await CmdHelper.RunCmdCommandsAsync("ipconfig");
+            Console.WriteLine(output);
+            //IMongoDatabase mongoDatabase = new MongoClient("mongodb://220.165.9.44:13398").GetDatabase("BeiDouDB");
+            //IMongoCollection<BsonDocument> collection = mongoDatabase.GetCollection<BsonDocument>("DeviceLocation");
+            //FilterDefinitionBuilder<BsonDocument> builder = Builders<BsonDocument>.Filter;
+            //var minDate = new DateTime(2018, 10, 1, 0, 0, 0);
+            //var maxDate = new DateTime(2018, 10, 26, 23, 59, 59);
+            //FilterDefinition<BsonDocument> filter = builder.Eq("DeviceID", Guid.Parse("808cbe9f-e08e-4f1d-82b4-07745089d484")) & builder.Gte("CreateTime", minDate) & builder.Lte("CreateTime", maxDate);
+            //var stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //List<BsonDocument> result = await collection.Find(filter).ToListAsync();
+            //stopwatch.Stop();
+            //Console.WriteLine($"{result.Count}条数据,耗时:{stopwatch.ElapsedTicks}");
         }
     }
 }
---
+++ Materal/Materal.WindowsHelper/CmdHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Materal.WindowsHelper
+{
+    public static class CmdHelper
+    {
+        /// <summary>
+        /// 运行命令提示符命令
+        /// </summary>
+        /// <param name="commands">命令组</param>
+        /// <returns>输出结果</returns>
+        public static async Task<string> RunCmdCommandsAsync(params string[] commands)
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                CreateNoWindow = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                Verb = "RunAs"
+            };
+            string output;
+            using (var process = new Process{StartInfo = processStartInfo })
+            {
+                process.Start();
+                foreach (string command in commands)
+                {
+                    await process.StandardInput.WriteLineAsync(command);
+                }
+                await process.StandardInput.WriteLineAsync("exit");
+                process.StandardInput.AutoFlush = true;
+                output = await process.StandardOutput.ReadToEndAsync();
+                process.WaitForExit();
+                process.Close();
+            }
+            return output;
+        }
+    }
+}
