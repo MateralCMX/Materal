5f8e164df6a1c44dc32d7aaf3e6e7b1e0c495025
陈明旭 <cloomcmx1554@hotmail.com>
2018-11-10 11:52:43 +0800

    1

---
+++ Test/NLogTest/NLogTest.UI/Program.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+using System;
+
+namespace NLogTest.UI
+{
+    public class Program
+    {
+        private static readonly Logger Logger = LogManager.GetLogger("File");
+        public static void Main()
+        {
+            Console.WriteLine("执行开始");
+            Logger.Error("Hello World");
+            Console.WriteLine("执行结束");
+            Console.ReadKey();
+        }
+    }
+}
