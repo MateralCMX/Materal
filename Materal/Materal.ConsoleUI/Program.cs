518a2cc773d55f9dcd0de24d68e77e41de7298ee
陈明旭 <cloomcmx1554@hotmail.com>
2019-03-27 14:47:26 +0800

    1、新增Http下载

--- Materal/Materal.ConsoleUI/Program.cs
+++ Materal/Materal.ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Data;
-using Materal.ExcelHelper;
+﻿using Materal.ExcelHelper;
+using Materal.NetworkHelper;
+using System.Data;
+using System.IO;
 
 namespace Materal.ConsoleUI
 {
@@ -8,7 +10,16 @@ namespace Materal.ConsoleUI
         public static void Main()
         {
             var excelManager = new ExcelManager();
-            DataSet excelData = excelManager.ReadExcelToDataSet(@"D:\Test.xlsx", 0);
+            byte[] body = HttpManager.SendGetBytes("http://192.168.0.125:8903/UploadFiles/敏感词.xlsx");
+            using (var fileStream = new FileStream("敏感词.xlsx", FileMode.OpenOrCreate))
+            {
+                using (var binaryWriter = new BinaryWriter(fileStream))
+                {
+                    binaryWriter.Write(body);
+                }
+            }
+            DataSet excelData = excelManager.ReadExcelToDataSet("敏感词.xlsx", 0);
+            File.Delete("敏感词.xlsx");
         }
     }
 }
