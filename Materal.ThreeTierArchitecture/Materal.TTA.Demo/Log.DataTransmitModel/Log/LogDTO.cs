8b73b998c3cb5ea81074923699d0ceb634f1a2e0
陈明旭 <cloomcmx1554@hotmail.com>
2019-06-03 21:03:43 +0800

    1、添加demo_web 2、添加log模型

--- Materal.ThreeTierArchitecture/Materal.TTA.Demo/Log.DataTransmitModel/Log/LogDTO.cs
+++ Materal.ThreeTierArchitecture/Materal.TTA.Demo/Log.DataTransmitModel/Log/LogDTO.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Log.DataTransmitModel.Log
+﻿namespace Log.DataTransmitModel.Log
 {
-    public class LogDTO
+    /// <summary>
+    /// 日志数据传输模型
+    /// </summary>
+    public class LogDTO : LogListDTO
     {
+        /// <summary>
+        /// 日志
+        /// </summary>
+        public string Logger { get; set; }
+        /// <summary>
+        /// 调用点
+        /// </summary>
+        public string Callsite { get; set; }
+        /// <summary>
+        /// 异常
+        /// </summary>
+        public string Exception { get; set; }
     }
 }
--- Materal.ThreeTierArchitecture/Materal.TTA.Demo/Log.Service/Model/Log/QueryLogFilterModel.cs
+++ Materal.ThreeTierArchitecture/Materal.TTA.Demo/Log.Service/Model/Log/QueryLogFilterModel.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Materal.Common;
+using System;
 
 namespace Log.Service.Model.Log
 {
-    public class QueryLogFilterModel
+    /// <summary>
+    /// 查询日志过滤器模型
+    /// </summary>
+    public class QueryLogFilterModel : PageRequestModel
     {
+        /// <summary>
+        /// 应用程序
+        /// </summary>
+        public string Application { get; set; }
+        /// <summary>
+        /// 等级
+        /// </summary>
+        public string Level { get; set; }
+        /// <summary>
+        /// 最小时间
+        /// </summary>
+        public DateTime? MinTime { get; set; }
+        /// <summary>
+        /// 最大时间
+        /// </summary>
+        public DateTime? MaxTime { get; set; }
     }
 }
