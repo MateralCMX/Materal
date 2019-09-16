402573bbafb2e6690ba381897aab9c0515286df1
陈明旭 <cloomcmx1554@hotmail.com>
2019-09-16 17:03:04 +0800

    1、新增WordHelper 0.0.1 2、更新包Materal.CacheHelper 1.0.3

---
+++ Materal/Materal.WordHelper.Example/ExampleByWordManager.cs
@@ -0,0 +1,87 @@
+﻿using Materal.WordHelper.Model;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace Materal.WordHelper.Example
+{
+    public class ExampleByWordManager
+    {
+        public void CreateTemplate()
+        {
+            var wordManager = new WordManager();
+            string documentPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/本年度支部工作计划-模板.docx");
+            XWPFDocument document = wordManager.ReadWord(documentPath);
+            var template = new List<TemplateModel>
+            {
+                new StringTemplateModel
+                {
+                    Key = "Content",
+                    Value = "这是年度工作计划的内容"
+                },
+                new StringTemplateModel
+                {
+                    Key = "Content2",
+                    Value = "~！@#￥%……&*（）——+内嵌模板!@#$%^&*()_+"
+                },
+                new StringTemplateModel
+                {
+                    Key = "Text1",
+                    Value = "表格内文本1"
+                },
+                new StringTemplateModel
+                {
+                    Key = "Text2",
+                    Value = "表格内文本2"
+                },
+                new StringTemplateModel
+                {
+                    Key = "Text3",
+                    Value = "表格内文本3"
+                },
+                new StringTemplateModel
+                {
+                    Key = "Text4",
+                    Value = "表格内文本4"
+                },
+                new TableTemplateModel
+                {
+                    Key = "Table",
+                    Value = GetData(),
+                    StartRowNumber = 2,
+                    OnSetCellText = null
+                }
+            };
+            wordManager.ApplyToTemplate(document, template.ToArray());
+            string documentSavePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/本年度支部工作计划.docx");
+            if (File.Exists(documentSavePath)) File.Delete(documentSavePath);
+            using (var fileStream = new FileStream(documentSavePath, FileMode.Create))
+            {
+                document.Write(fileStream);
+            }
+        }
+
+        private DataTable GetData()
+        {
+            var dataTable = new DataTable
+            {
+                TableName = "Table1"
+            };
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("Age");
+            dataTable.Columns.Add("Remark");
+            for (var i = 0; i < 10; i++)
+            {
+                DataRow dataRow = dataTable.NewRow();
+                dataRow[0] = $"Materal{i}";
+                dataRow[1] = $"{i + 20}岁";
+                dataRow[2] = "这是备注";
+                dataTable.Rows.Add(dataRow);
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ Materal/Materal.WordHelper/Model/TemplateModel.cs
@@ -0,0 +1,45 @@
+﻿using NPOI.XWPF.UserModel;
+using System;
+using System.Data;
+
+namespace Materal.WordHelper.Model
+{
+    /// <summary>
+    /// 模板模型
+    /// </summary>
+    public class TemplateModel
+    {
+        /// <summary>
+        /// 键
+        /// </summary>
+        public string Key { get; set; }
+    }
+    /// <summary>
+    /// 字符串模板模型
+    /// </summary>
+    public class StringTemplateModel : TemplateModel
+    {
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value { get; set; }
+    }
+    /// <summary>
+    /// 表格模板模型
+    /// </summary>
+    public class TableTemplateModel : TemplateModel
+    {
+        /// <summary>
+        /// 值
+        /// </summary>
+        public DataTable Value { get; set; }
+        /// <summary>
+        /// 开始行数
+        /// </summary>
+        public int StartRowNumber { get; set; }
+        /// <summary>
+        /// 设置单元格
+        /// </summary>
+        public Func<int ,int, XWPFParagraph, XWPFRun> OnSetCellText { get; set; }
+    }
+}
