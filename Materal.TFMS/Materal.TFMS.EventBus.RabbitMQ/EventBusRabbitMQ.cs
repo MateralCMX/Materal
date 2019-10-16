8500f3a1b3c2a2a8c4c965abbb82d52cfaa8d41f
陈明旭 <cloomcmx1554@hotmail.com>
2019-10-16 10:47:47 +0800

    1

--- Materal.TFMS/Materal.TFMS.EventBus.RabbitMQ/EventBusRabbitMQ.cs
+++ Materal.TFMS/Materal.TFMS.EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -204,6 +204,7 @@ namespace Materal.TFMS.EventBus.RabbitMQ
             catch (Exception ex)
             {
                 _logger?.LogError(ex, "错误消息: \"{Message}\"", message);
+                _consumerChannel.BasicNack(eventArgs.DeliveryTag, false, false);
             }
         }
         /// <summary>
