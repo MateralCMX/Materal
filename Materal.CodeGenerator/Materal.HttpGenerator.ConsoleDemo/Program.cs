1470ac23768561bddb955bf6215662980d8bc9c4
陈明旭 <342860484@qq.com>
2022-11-18 18:03:14 +0800

    feat:新增控制台工具

--- Materal.CodeGenerator/Materal.HttpGenerator.ConsoleDemo/Program.cs
+++ Materal.CodeGenerator/Materal.HttpGenerator.ConsoleDemo/Program.cs
@@ -12,9 +12,14 @@ namespace Materal.HttpGenerator.ConsoleDemo
             if (string.IsNullOrWhiteSpace(path)) return;
             Console.WriteLine("请输入项目名称");
             generator.ProjectName = Console.ReadLine() ?? "Test";
+            Console.WriteLine("正在获取Swagger数据......");
+            await generator.SetSourceAsync(path);
             //generator.ProjectName = "Educational";
             //await generator.SetSourceAsync("https://oaapi.xmjriyu.com/swagger/v1/swagger.json");
+            Console.WriteLine("开始构建......");
             await generator.BuildAsync();
+            Console.WriteLine("构建完毕,按任意键退出。");
+            Console.Read();
         }
     }
 }
