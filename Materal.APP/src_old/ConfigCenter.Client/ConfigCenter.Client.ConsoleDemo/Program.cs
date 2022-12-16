1358e6c7d4a0105648a076a10219ab0e18e28255
陈明旭 <342860484@qq.com>
2022-12-16 14:34:47 +0800

    feat:优化配置中心客户端使用方法

--- Materal.APP/src_old/ConfigCenter.Client/ConfigCenter.Client.ConsoleDemo/Program.cs
+++ Materal.APP/src_old/ConfigCenter.Client/ConfigCenter.Client.ConsoleDemo/Program.cs
@@ -7,14 +7,14 @@ namespace ConfigCenter.Client.ConsoleDemo
     {
         public static async Task Main()
         {
-            IMateralConfigurationBuilder configurationBuilder = new MateralConfigurationBuilder("http://175.27.194.19:8720", "XMJProject", -1)
-                .AddDefaultNamespace()
-                .AddNamespace("WebAPI")
-                .AddNamespace("Demo");
-            IConfigurationRoot _configuration = configurationBuilder.Build();
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .AddDefaultNameSpace("http://175.27.194.19:8720", "XMJProject", 60)
+                .AddNameSpace("WebAPI")
+                .AddNameSpace("Demo");
+            IConfiguration _configuration = configurationBuilder.Build();
             while (true)
             {
-                string? testValue = _configuration.GetValue("MateralLogger:Application");
+                string? testValue = _configuration.GetValue("AppName");
                 Console.WriteLine(testValue);
                 await Task.Delay(1000);
             }
