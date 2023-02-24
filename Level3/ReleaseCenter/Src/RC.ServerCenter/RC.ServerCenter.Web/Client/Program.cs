93d69c6486fb8410193917f95745a55d15eb5bcd
陈明旭 <342860484@qq.com>
2023-02-24 18:19:56 +0800

    feat:添加Web页面项目

--- Level3/ReleaseCenter/Src/RC.ServerCenter/RC.ServerCenter.Web/Client/Program.cs
+++ Level3/ReleaseCenter/Src/RC.ServerCenter/RC.ServerCenter.Web/Client/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using RC.ServerCenter.Web;
 
 namespace RC.ServerCenter.Web
 {
@@ -8,12 +7,10 @@ namespace RC.ServerCenter.Web
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped(sp => new System.Net.Http.HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
     }
