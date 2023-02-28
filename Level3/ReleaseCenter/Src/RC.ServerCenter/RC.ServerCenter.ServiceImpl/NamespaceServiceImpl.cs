2a75af52e42dbc90856a57c7009cb72f90721e20
陈明旭 <342860484@qq.com>
2023-02-28 18:16:40 +0800

    fix:修复同一项目下命名空间不可重复bug

--- Level3/ReleaseCenter/Src/RC.ServerCenter/RC.ServerCenter.ServiceImpl/NamespaceServiceImpl.cs
+++ Level3/ReleaseCenter/Src/RC.ServerCenter/RC.ServerCenter.ServiceImpl/NamespaceServiceImpl.cs
@@ -15,7 +15,7 @@ namespace RC.ServerCenter.ServiceImpl
         }
         public override async Task<Guid> AddAsync(AddNamespaceModel model)
         {
-            if (await DefaultRepository.ExistedAsync(m => m.Name == model.Name)) throw new RCException("名称重复");
+            if (await DefaultRepository.ExistedAsync(m => m.ProjectID == model.ProjectID && m.Name == model.Name)) throw new RCException("名称重复");
             return await base.AddAsync(model);
         }
         protected override async Task DeleteAsync(Namespace domain)
