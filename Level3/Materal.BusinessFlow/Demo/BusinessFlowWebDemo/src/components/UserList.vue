eccb864f529632067f80abb4735b5818c7f45b6d
陈明旭 <342860484@qq.com>
2023-05-11 10:08:33 +0800

    feat:[业务流]WebDemo组件按需加载

--- Level3/Materal.BusinessFlow/Demo/BusinessFlowWebDemo/src/components/UserList.vue
+++ Level3/Materal.BusinessFlow/Demo/BusinessFlowWebDemo/src/components/UserList.vue
@@ -1,38 +1,49 @@
 <template>
     <a-space direction="vertical" style="width: 100%;">
-        <a-form layout="inline" :model="queryData" @finish="searchData">
+        <a-form layout="inline" :model="_queryData" @finish="SearchData">
             <a-form-item>
-                <a-input v-model:value="queryData.Name" placeholder="名称" />
+                <a-input v-model:value="_queryData.Name" placeholder="名称" allow-clear />
             </a-form-item>
             <a-form-item>
-                <a-button type="primary" html-type="sumbit" :loading="searching">查询</a-button>
+                <a-button type="primary" html-type="submit" :loading="_searching">查询</a-button>
             </a-form-item>
         </a-form>
         <div>
-            <a-button type="primary" :loading="searching">添加</a-button>
+            <a-button type="primary" :loading="_searching" @click="OpenOptionDrawer">添加</a-button>
         </div>
-        <a-table :columns="tableColumns" :data-source="tableData" :loading="searching" :pagination="tablePagination"
-            @change="tableChange"></a-table>
+        <a-table :columns="_tableColumns" :data-source="_tableData" :loading="_searching" :pagination="TablePagination"
+            @change="TableChange"></a-table>
     </a-space>
+    <a-drawer v-model:visible="_optionDrawerVisible" title="用户操作">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+    </a-drawer>
 </template>
 <script setup lang="ts">
 import { computed, onMounted, reactive, ref } from 'vue';
+import { TablePaginationConfig } from 'ant-design-vue';
 import { QueryUserModel } from '../models/User/QueryUserModel';
 import { User } from '../models/User/User';
 import { PageModel } from '../models/PageModel';
 import UserService from '../services/UserService';
+
 /**
  * 查询数据
  */
-const queryData = reactive<QueryUserModel>(new QueryUserModel());
+const _queryData = reactive<QueryUserModel>(new QueryUserModel());
 /**
  * 分页数据
  */
-const pageInfo = ref<PageModel>(new PageModel());
+const _pageInfo = ref<PageModel>(new PageModel());
+/**
+ * 操作抽屉是否显示
+ */
+const _optionDrawerVisible = ref(false);
 /**
  * 表格列
  */
-const tableColumns = [
+const _tableColumns = [
     {
         title: '姓名',
         dataIndex: 'Name',
@@ -46,42 +57,48 @@ const tableColumns = [
 /**
  * 表格数据
  */
-const tableData = ref<User[]>([]);
+const _tableData = ref<User[]>([]);
 /**
  * 查询中标识
  */
-const searching = ref(false);
+const _searching = ref(false);
+/**
+ * 打开操作抽屉
+ */
+const OpenOptionDrawer = () => {
+    _optionDrawerVisible.value = true;
+};
 /**
  * 检索数据
  */
-const searchData = async () => {
-    searching.value = true;
-    const result = await UserService.GetListAsync(queryData);
+const SearchData = async () => {
+    _searching.value = true;
+    const result = await UserService.GetListAsync(_queryData);
     if (result) {
-        tableData.value = result.Data;
-        pageInfo.value = result.PageModel;
+        _tableData.value = result.Data;
+        _pageInfo.value = result.PageModel;
     }
-    searching.value = false;
+    _searching.value = false;
 }
 /**
  * 表格数据加载完毕事件
  */
-const tablePagination = computed(() => ({
-    total: pageInfo.value.DataCount,
-    current: pageInfo.value.PageIndex,
-    pageSize: pageInfo.value.PageSize
+const TablePagination = computed(() => ({
+    total: _pageInfo.value.DataCount,
+    current: _pageInfo.value.PageIndex,
+    pageSize: _pageInfo.value.PageSize
 }));
 /**
  * 表格更改事件
  * @param pageinfo 分页信息
  */
-const tableChange = (pageinfo: { pageSize: number; current: number }) => {
-    queryData.PageIndex = pageinfo.current;
-    queryData.PageSize = pageinfo.pageSize;
-    searchData();
+const TableChange = (pagination: TablePaginationConfig) => {
+    _queryData.PageIndex = pagination.current ?? 1;
+    _queryData.PageSize = pagination.pageSize ?? 10;
+    SearchData();
 };
 /**
  * 页面加载完毕事件
  */
-onMounted(() => { searchData(); });
+onMounted(() => { SearchData(); });
 </script>
--- Level3/Materal.BusinessFlow/Demo/BusinessFlowWebDemo/src/main.ts
+++ Level3/Materal.BusinessFlow/Demo/BusinessFlowWebDemo/src/main.ts
@@ -1,9 +1,7 @@
 import './style.css'
-import 'ant-design-vue/dist/antd.css';
 import { router } from "./router";
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue';
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 const app = createApp(App);
-app.use(router).use(Antd).mount('#app');
+app.use(router).mount('#app');
