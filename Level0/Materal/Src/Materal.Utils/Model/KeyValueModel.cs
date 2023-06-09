86912b3fb73e17e8a22e521347af9493fd85e703
陈明旭 <342860484@qq.com>
2023-06-09 15:33:27 +0800

    feat:[Utils]新增KeyValueModel<TKey,TValue>

--- Level0/Materal/Src/Materal.Utils/Model/KeyValueModel.cs
+++ Level0/Materal/Src/Materal.Utils/Model/KeyValueModel.cs
@@ -6,43 +6,78 @@
     public class KeyValueModel
     {
         /// <summary>
+        /// 键
+        /// </summary>
+        public string Key { get; }
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value { get; }
+        /// <summary>
         /// 构造方法
         /// </summary>
         /// <param name="key">键</param>
         /// <param name="value">值</param>
-        public KeyValueModel(int key, string value)
+        public KeyValueModel(string key, string value)
         {
             Key = key;
             Value = value;
         }
         /// <summary>
-        /// 键
+        /// 构造方法
         /// </summary>
-        public int Key { get; }
-        /// <summary>
-        /// 值
-        /// </summary>
-        public string Value { get; }
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        public KeyValueModel(int key, string value) : this(key.ToString(), value)
+        {
+        }
         /// <summary>
         /// 获得所有识别码
         /// </summary>
         /// <returns>所有识别码</returns>
         public static List<KeyValueModel> GetAllCode(Type enumType)
         {
-            var allCodeList = enumType.GetAllEnum();
-            var resM = new List<KeyValueModel>();
-            foreach (Enum item in allCodeList)
-            {
-                resM.Add(new KeyValueModel(Convert.ToInt32(item), item.GetDescription()));
-            }
-            return resM;
+            List<Enum> allCodeList = enumType.GetAllEnum();
+            List<KeyValueModel> result = allCodeList.Select(item => new KeyValueModel(Convert.ToInt32(item), item.GetDescription())).ToList();
+            return result;
         }
     }
     /// <summary>
     /// 键值对模型
     /// </summary>
-    public class KeyValueModel<TEnum> where TEnum : struct, Enum
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class KeyValueModel<TKey, TValue>
     {
+        /// <summary>
+        /// 键
+        /// </summary>
+        public TKey? Key { get; set; }
+        /// <summary>
+        /// 值
+        /// </summary>
+        public TValue? Value { get; set; }
+    }
+    /// <summary>
+    /// 键值对模型
+    /// </summary>
+    public class KeyValueModel<TEnum>
+        where TEnum : struct, Enum
+    {
+        /// <summary>
+        /// 键
+        /// </summary>
+        public TEnum Key { get; }
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value { get; } = string.Empty;
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public KeyValueModel()
+        {
+        }
         /// <summary>
         /// 构造方法
         /// </summary>
@@ -54,27 +89,15 @@
             Value = value;
         }
         /// <summary>
-        /// 键
-        /// </summary>
-        public TEnum Key { get; }
-        /// <summary>
-        /// 值
-        /// </summary>
-        public string Value { get; }
-        /// <summary>
         /// 获得所有识别码
         /// </summary>
         /// <returns>所有识别码</returns>
         public static List<KeyValueModel<TEnum>> GetAllCode()
         {
             Type enumType = typeof(TEnum);
-            var allCodeList = enumType.GetAllEnum();
-            var resM = new List<KeyValueModel<TEnum>>();
-            foreach (Enum item in allCodeList)
-            {
-                resM.Add(new KeyValueModel<TEnum>((TEnum)item, item.GetDescription()));
-            }
-            return resM;
+            List<Enum> allCodeList = enumType.GetAllEnum();
+            List<KeyValueModel<TEnum>> result = allCodeList.Select(item => new KeyValueModel<TEnum>((TEnum)item, item.GetDescription())).ToList();
+            return result;
         }
     }
 }
---
+++ Level0/Materal/Test/Materal.Test/UtilsTests/KeyValueModelTest.cs
@@ -0,0 +1,17 @@
+﻿using Materal.Utils.Model;
+
+namespace Materal.Test.UtilsTests
+{
+    [TestClass]
+    public class KeyValueModelTest : BaseTest
+    {
+        public void KeyValueModelTest1() 
+        {
+            KeyValueModel<string, string> keyValueModel = new()
+            {
+                Key = "key1",
+                Value = "value1"
+            };
+        }
+    }
+}
