3fd8d6b576be96e6d51022ae7ef1d3e0523c665b
陈明旭 <342860484@qq.com>
2023-06-09 16:51:23 +0800

    feat:[Utils]添加KeyValueModel扩展方法

--- Level0/Materal/Src/Materal.Utils/Model/KeyValueModel.cs
+++ Level0/Materal/Src/Materal.Utils/Model/KeyValueModel.cs
@@ -16,30 +16,20 @@
         /// <summary>
         /// 构造方法
         /// </summary>
-        /// <param name="key">键</param>
-        /// <param name="value">值</param>
-        public KeyValueModel(string key, string value)
+        public KeyValueModel()
         {
-            Key = key;
-            Value = value;
+            Key = string.Empty;
+            Value = string.Empty;
         }
         /// <summary>
         /// 构造方法
         /// </summary>
         /// <param name="key">键</param>
         /// <param name="value">值</param>
-        public KeyValueModel(int key, string value) : this(key.ToString(), value)
+        public KeyValueModel(string key, string value)
         {
-        }
-        /// <summary>
-        /// 获得所有识别码
-        /// </summary>
-        /// <returns>所有识别码</returns>
-        public static List<KeyValueModel> GetAllCode(Type enumType)
-        {
-            List<Enum> allCodeList = enumType.GetAllEnum();
-            List<KeyValueModel> result = allCodeList.Select(item => new KeyValueModel(Convert.ToInt32(item), item.GetDescription())).ToList();
-            return result;
+            Key = key;
+            Value = value;
         }
     }
     /// <summary>
@@ -57,10 +47,27 @@
         /// 值
         /// </summary>
         public TValue? Value { get; set; }
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public KeyValueModel()
+        {
+        }
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        public KeyValueModel(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
     }
     /// <summary>
     /// 键值对模型
     /// </summary>
+    /// <typeparam name="TEnum"></typeparam>
     public class KeyValueModel<TEnum>
         where TEnum : struct, Enum
     {
@@ -82,6 +89,15 @@
         /// 构造方法
         /// </summary>
         /// <param name="key">键</param>
+        public KeyValueModel(TEnum key)
+        {
+            Key = key;
+            Value = key.ToString();
+        }
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="key">键</param>
         /// <param name="value">值</param>
         public KeyValueModel(TEnum key, string value)
         {
@@ -95,6 +111,70 @@
         public static List<KeyValueModel<TEnum>> GetAllCode()
         {
             Type enumType = typeof(TEnum);
+            return enumType.GetAllCode<TEnum>();
+        }
+    }
+    /// <summary>
+    /// 键值对模型扩展
+    /// </summary>
+    public static class KeyValueModelExtension
+    {
+        /// <summary>
+        /// 转换为字典
+        /// </summary>
+        /// <param name="keyValueModels"></param>
+        /// <param name="useNewValue">如果Key重复,使用新的值</param>
+        /// <returns></returns>
+        public static Dictionary<string, string> ToDictionary(this List<KeyValueModel> keyValueModels, bool useNewValue = true)
+        {
+            Dictionary<string, string> result = new();
+            foreach (KeyValueModel item in keyValueModels)
+            {
+                if (result.ContainsKey(item.Key))
+                {
+                    if (useNewValue)
+                    {
+                        result[item.Key] = item.Value;
+                    }
+                }
+                else
+                {
+                    result.Add(item.Key, item.Value);
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// 转换为键值对模型
+        /// </summary>
+        /// <param name="dictionary"></param>
+        /// <returns></returns>
+        public static List<KeyValueModel> ToKeyValueModel(this Dictionary<string, string> dictionary) 
+            => dictionary.Select(item => new KeyValueModel(item.Key, item.Value)).ToList();
+        /// <summary>
+        /// 转换为键值对模型
+        /// </summary>
+        /// <param name="dictionary"></param>
+        /// <returns></returns>
+        public static List<KeyValueModel<TKey, TValue>> ToKeyValueModel<TKey,TValue>(this Dictionary<TKey, TValue> dictionary)
+            => dictionary.Select(item => new KeyValueModel<TKey, TValue>(item.Key, item.Value)).ToList();
+        /// <summary>
+        /// 获得所有识别码
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static List<KeyValueModel<TEnum>> GetAllCode<TEnum>(this TEnum @enum)
+            where TEnum : struct, Enum => @enum.GetType().GetAllCode<TEnum>();
+        /// <summary>
+        /// 获得所有识别码
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enumType"></param>
+        /// <returns></returns>
+        internal static List<KeyValueModel<TEnum>> GetAllCode<TEnum>(this Type enumType)
+            where TEnum : struct, Enum
+        {
             List<Enum> allCodeList = enumType.GetAllEnum();
             List<KeyValueModel<TEnum>> result = allCodeList.Select(item => new KeyValueModel<TEnum>((TEnum)item, item.GetDescription())).ToList();
             return result;
