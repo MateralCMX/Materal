40035ff0d8d51fc62cd2ed96366f864d1cb7e6d0
陈明旭 <342860484@qq.com>
2023-08-26 15:31:34 +0800

    feat:[日志]新增Asp.Net支持

---
+++ Level1/Materal.Logger/Src/Materal.Logger.AspNetCore/LoggerDIExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Materal.Logger
+{
+    /// <summary>
+    /// DI扩展
+    /// </summary>
+    public static class LoggerDIExtension
+    {
+        /// <summary>
+        /// 使用日志服务
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseMateralLogger(this IApplicationBuilder app, IConfiguration? configuration = null)
+        {
+            LoggerManager.Init(configuration);
+            return app;
+        }
+    }
+}
