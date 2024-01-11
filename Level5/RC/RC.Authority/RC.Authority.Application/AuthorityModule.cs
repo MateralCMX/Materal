9301deb50e82d5389029583bc40521fcd3d145e5
Materal <cloomcmx1554@hotmail.com>
2024-01-11 21:26:48 +0800

    feat:添加默认用户迁移文件

--- Level5/RC/RC.Authority/RC.Authority.Application/AuthorityModule.cs
+++ Level5/RC/RC.Authority/RC.Authority.Application/AuthorityModule.cs
@@ -15,18 +15,5 @@
             context.Services.Configure<ApplicationConfig>(context.Configuration);
             await base.OnConfigServiceAsync(context);
         }
-        /// <summary>
-        /// 应用程序初始化
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public override async Task OnApplicationInitAsync(IApplicationContext context)
-        {
-            using IServiceScope scope = context.ServiceProvider.CreateScope();
-            IUserService userService = scope.ServiceProvider.GetRequiredService<IUserService>();
-            userService.ServiceProvider = scope.ServiceProvider;
-            await userService.AddDefaultUserAsync();
-            await base.OnApplicationInitAsync(context);
-        }
     }
 }
--- Level5/RC/RC.Authority/RC.Authority.Repository/Migrations/20240111131414_AddDefaultUser.Designer.cs
+++ Level5/RC/RC.Authority/RC.Authority.Repository/Migrations/20240111131414_AddDefaultUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RC.Authority.Repository;
 
@@ -10,9 +11,11 @@ using RC.Authority.Repository;
 namespace RC.Authority.Repository.Migrations
 {
     [DbContext(typeof(AuthorityDBContext))]
-    partial class AuthorityDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240111131414_AddDefaultUser")]
+    partial class AddDefaultUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
