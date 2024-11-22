fb63a9c943a249940d97f0b5bda87def8af5464e
Materal <cloomcmx1554@hotmail.com>
2024-11-22 18:13:33 +0800

    feat:新增更改二维码方法

--- Materal/Src/Materal.Utils.BarCode/QRCodeHelper.cs
+++ Materal/Src/Materal.Utils.BarCode/QRCodeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using SkiaSharp;
+using System.Drawing;
 using ZXing;
 using ZXing.Common;
 using ZXing.SkiaSharp;
@@ -46,79 +47,6 @@ namespace Materal.Utils.BarCode
             return result;
         }
         /// <summary>
-        /// 创建二维码
-        /// </summary>
-        /// <param name="content"></param>
-        /// <param name="size"></param>
-        /// <param name="foregroundColor"></param>
-        /// <param name="backgroundColor"></param>
-        /// <returns></returns>
-        public static SKBitmap CreateQRCode(string content, int size, SKColor foregroundColor, SKColor backgroundColor) => CreateQRCode(content, size, size, foregroundColor, backgroundColor);
-        /// <summary>
-        /// 创建二维码
-        /// </summary>
-        /// <param name="content"></param>
-        /// <param name="heigth"></param>
-        /// <param name="width"></param>
-        /// <param name="foregroundColor"></param>
-        /// <param name="backgroundColor"></param>
-        /// <returns></returns>
-        public static SKBitmap CreateQRCode(string content, int heigth, int width, SKColor foregroundColor, SKColor backgroundColor) => CreateQRCode(content, new EncodingOptions
-        {
-            Height = heigth,
-            Width = width
-        }, foregroundColor, backgroundColor);
-        /// <summary>
-        /// 创建带颜色的二维码
-        /// </summary>
-        /// <param name="content"></param>
-        /// <param name="encodingOptions"></param>
-        /// <param name="foregroundColor"></param>
-        /// <param name="backgroundColor"></param>
-        /// <returns></returns>
-        public static SKBitmap CreateQRCode(string content, EncodingOptions encodingOptions, SKColor foregroundColor, SKColor backgroundColor)
-        {
-            SKBitmap qrCode = CreateQRCode(content, encodingOptions);
-            SKBitmap result = ColorReplace(qrCode, foregroundColor, backgroundColor);
-            return result;
-        }
-        /// <summary>
-        /// 创建图片二维码
-        /// </summary>
-        /// <param name="content"></param>
-        /// <param name="foregroundImage"></param>
-        /// <param name="backgroundColor"></param>
-        /// <returns></returns>
-        public static SKBitmap CreateQRCode(string content, SKBitmap foregroundImage, SKColor backgroundColor)
-        {
-            EncodingOptions encodingOptions = new()
-            {
-                Width = foregroundImage.Width,
-                Height = foregroundImage.Height
-            };
-            SKBitmap qrCode = CreateQRCode(content, encodingOptions);
-            SKBitmap result = ImageReplace(qrCode, foregroundImage, backgroundColor);
-            return result;
-        }
-        /// <summary>
-        /// 创建图片二维码
-        /// </summary>
-        /// <param name="content"></param>
-        /// <param name="foregroundColor"></param>
-        /// <param name="backgroundImage"></param>
-        /// <returns></returns>
-        public static SKBitmap CreateQRCode(string content, SKColor foregroundColor, SKBitmap backgroundImage)
-        {
-            EncodingOptions encodingOptions = new()
-            {
-                Width = backgroundImage.Width,
-                Height = backgroundImage.Height
-            };
-            SKBitmap qrCode = CreateQRCode(content, encodingOptions);
-            SKBitmap result = ImageReplace(qrCode, foregroundColor, backgroundImage);
-            return result;
-        }
-        /// <summary>
         /// 读取二维码
         /// </summary>
         /// <param name="bitmap"></param>
@@ -126,97 +54,203 @@ namespace Materal.Utils.BarCode
         /// <exception cref="UtilException"></exception>
         public static string ReadQRCode(SKBitmap bitmap)
         {
-            try
-            {
-                string result = BarCodeHelper.ReadBarCode(bitmap, out BarcodeFormat barcodeFormat);
-                if (barcodeFormat != BarcodeFormat.QR_CODE) throw new UtilException("图片不是二维码");
-                return result;
-            }
-            catch
-            {
-                SKBitmap blackWhiteQRCode = ColorReplace(bitmap, SKColors.Black, SKColors.White);
-                string result = BarCodeHelper.ReadBarCode(blackWhiteQRCode, out BarcodeFormat barcodeFormat);
-                if (barcodeFormat != BarcodeFormat.QR_CODE) throw new UtilException("图片不是二维码");
-                return result;
-            }
+            string result = BarCodeHelper.ReadBarCode(bitmap, out BarcodeFormat barcodeFormat);
+            if (barcodeFormat != BarcodeFormat.QR_CODE) throw new UtilException("图片不是二维码");
+            return result;
         }
         /// <summary>
-        /// 替换颜色
+        /// 更改二维码图片
         /// </summary>
         /// <param name="qrCode"></param>
-        /// <param name="foregroundColor"></param>
-        /// <param name="backgroundColor"></param>
+        /// <param name="drawAction"></param>
+        /// <param name="setMarkPaint"></param>
+        /// <param name="background"></param>
         /// <returns></returns>
-        public static SKBitmap ColorReplace(SKBitmap qrCode, SKColor foregroundColor, SKColor backgroundColor)
+        public static SKBitmap ChangeQRCodeImage(SKBitmap qrCode, Action<SKCanvas, SKPaint, Point, Size> drawAction, Action<SKPaint, Point>? setMarkPaint = null, SKColor? background = null)
         {
+            Point leftTopPoint = GetQRCodeLeftTopPoint(qrCode);
+            Size pointSize = GetQRCodePointSize(qrCode, leftTopPoint);
+            QRCodeMark leftTopMark = GetQRCodeLeftTopMark(qrCode, pointSize);
+            QRCodeMark rightTopMark = GetQrCodeRigghtTopMark(qrCode, pointSize);
+            QRCodeMark leftBottomMark = GetQRCodeLeftBottomMark(qrCode, pointSize);
             SKBitmap result = new(qrCode.Width, qrCode.Height);
+            SKColor oldBackgroundColor = qrCode.GetPixel(0, 0);
             using SKCanvas canvas = new(result);
-            canvas.Clear(backgroundColor);
+            canvas.Clear(background ?? oldBackgroundColor);
             using SKPaint paint = new();
-            SKColor? oldBackgroundColor = null;
-            for (int x = 0; x < qrCode.Width; x++)
+            for (int x = leftTopPoint.X; x < qrCode.Width - leftTopPoint.X; x += pointSize.Width)
             {
-                for (int y = 0; y < qrCode.Height; y++)
+                for (int y = leftTopPoint.Y; y < qrCode.Height - leftTopPoint.Y; y += pointSize.Height)
                 {
-                    oldBackgroundColor ??= qrCode.GetPixel(x, y);
-                    if (qrCode.GetPixel(x, y) == oldBackgroundColor) continue;
-                    paint.Color = foregroundColor;
-                    canvas.DrawPoint(x, y, paint);
+                    SKColor nowColor = qrCode.GetPixel(x, y);
+                    if (nowColor == oldBackgroundColor) continue;
+                    paint.Color = nowColor;
+                    Point point = new(x, y);
+                    Point tempLeftTopPoint = new(x, y);
+                    Point tempRightBottomPoint = new(x + pointSize.Width, y + pointSize.Height);
+                    float centerX = (tempLeftTopPoint.X + tempRightBottomPoint.X) / 2;
+                    float centerY = (tempLeftTopPoint.Y + tempRightBottomPoint.Y) / 2;
+                    if (!PointInMarker(point, leftTopMark, rightTopMark, leftBottomMark))
+                    {
+                        drawAction(canvas, paint, new Point((int)centerX, (int)centerY), new Size(pointSize.Width, pointSize.Height));
+                    }
+                    else
+                    {
+                        setMarkPaint?.Invoke(paint, new Point((int)centerX, (int)centerY));
+                        canvas.DrawRect(tempLeftTopPoint.X, tempLeftTopPoint.Y, pointSize.Width, pointSize.Height, paint);
+                    }
                 }
             }
             return result;
         }
         /// <summary>
-        /// 替换图片
+        /// 获得二维码左上角点
         /// </summary>
         /// <param name="qrCode"></param>
-        /// <param name="foregroundImage"></param>
-        /// <param name="backgroundColor"></param>
         /// <returns></returns>
-        public static SKBitmap ImageReplace(SKBitmap qrCode, SKBitmap foregroundImage, SKColor backgroundColor)
+        /// <exception cref="UtilException"></exception>
+        private static Point GetQRCodeLeftTopPoint(SKBitmap qrCode)
         {
-            SKBitmap result = new(qrCode.Width, qrCode.Height);
-            using SKCanvas canvas = new(result);
-            canvas.Clear(backgroundColor);
-            using SKPaint paint = new();
-            SKColor? oldBackgroundColor = null;
             for (int x = 0; x < qrCode.Width; x++)
             {
                 for (int y = 0; y < qrCode.Height; y++)
                 {
-                    oldBackgroundColor ??= qrCode.GetPixel(x, y);
-                    if (qrCode.GetPixel(x, y) == oldBackgroundColor) continue;
-                    paint.Color = foregroundImage.GetPixel(x, y);
-                    canvas.DrawPoint(x, y, paint);
+                    if (qrCode.GetPixel(x, y) == SKColors.White) continue;
+                    return new Point(x, y);
                 }
             }
-            return result;
+            throw new UtilException("未找到左上角点");
         }
         /// <summary>
-        /// 替换图片
+        /// 获得二维码左下角点
         /// </summary>
         /// <param name="qrCode"></param>
-        /// <param name="foregroundColor"></param>
-        /// <param name="backgroundImage"></param>
         /// <returns></returns>
-        public static SKBitmap ImageReplace(SKBitmap qrCode, SKColor foregroundColor, SKBitmap backgroundImage)
+        /// <exception cref="UtilException"></exception>
+        private static Point GetQRCodeLeftBottomPoint(SKBitmap qrCode)
         {
-            SKBitmap result = new(qrCode.Width, qrCode.Height);
-            using SKCanvas canvas = new(result);
-            canvas.Clear(foregroundColor);
-            using SKPaint paint = new();
-            SKColor? oldBackgroundColor = null;
             for (int x = 0; x < qrCode.Width; x++)
+            {
+                for (int y = qrCode.Height - 1; y >= 0; y--)
+                {
+                    if (qrCode.GetPixel(x, y) == SKColors.White) continue;
+                    return new Point(x, y);
+                }
+            }
+            throw new UtilException("未找到左下角点");
+        }
+        /// <summary>
+        /// 获得二维码右上角点
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <returns></returns>
+        /// <exception cref="UtilException"></exception>
+        private static Point GetQRCodeRightTopPoint(SKBitmap qrCode)
+        {
+            for (int x = qrCode.Width - 1; x >= 0; x--)
             {
                 for (int y = 0; y < qrCode.Height; y++)
                 {
-                    oldBackgroundColor ??= qrCode.GetPixel(x, y);
-                    if (qrCode.GetPixel(x, y) != oldBackgroundColor) continue;
-                    paint.Color = backgroundImage.GetPixel(x, y);
-                    canvas.DrawPoint(x, y, paint);
+                    if (qrCode.GetPixel(x, y) == SKColors.White) continue;
+                    return new Point(x, y);
                 }
             }
-            return result;
+            throw new UtilException("未找到右上角点");
+        }
+        /// <summary>
+        /// 获得二维码左上标记
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <param name="pointSize"></param>
+        /// <returns></returns>
+        private static QRCodeMark GetQRCodeLeftTopMark(SKBitmap qrCode, Size pointSize)
+        {
+            Point leftTopPoint = GetQRCodeLeftTopPoint(qrCode);
+            return new()
+            {
+                LeftTopPoint = leftTopPoint,
+                RightBottomPoint = new(leftTopPoint.X + pointSize.Width * 7, leftTopPoint.Y + pointSize.Height * 7)
+            };
+        }
+        /// <summary>
+        /// 获得二维码右上标记
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <param name="pointSize"></param>
+        /// <returns></returns>
+        private static QRCodeMark GetQrCodeRigghtTopMark(SKBitmap qrCode, Size pointSize)
+        {
+            Point rightTopPoint = GetQRCodeRightTopPoint(qrCode);
+            return new()
+            {
+                LeftTopPoint = new(rightTopPoint.X - pointSize.Width * 7, rightTopPoint.Y),
+                RightBottomPoint = new(rightTopPoint.X, rightTopPoint.Y + pointSize.Height * 7)
+            };
+        }
+        /// <summary>
+        /// 获得二维码左下标记
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <param name="pointSize"></param>
+        /// <returns></returns>
+        private static QRCodeMark GetQRCodeLeftBottomMark(SKBitmap qrCode, Size pointSize)
+        {
+            Point leftBottomPoint = GetQRCodeLeftBottomPoint(qrCode);
+            return new()
+            {
+                LeftTopPoint = new(leftBottomPoint.X, leftBottomPoint.Y - pointSize.Height * 7),
+                RightBottomPoint = new(leftBottomPoint.X + pointSize.Width * 7, leftBottomPoint.Y)
+            };
+        }
+        /// <summary>
+        /// 获取二维码点大小
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <param name="leftTopPoint"></param>
+        /// <returns></returns>
+        private static Size GetQRCodePointSize(SKBitmap qrCode, Point leftTopPoint)
+        {
+            SKColor foregroundColor = qrCode.GetPixel(leftTopPoint.X, leftTopPoint.Y);
+            int x = leftTopPoint.X;
+            int y = leftTopPoint.Y;
+            for (int i = 1; i < qrCode.Width; i++)
+            {
+                if (qrCode.GetPixel(x + i, y + i) == foregroundColor) continue;
+                return new Size(i, i);
+            }
+            throw new UtilException("未找到二维码点大小");
+        }
+        /// <summary>
+        /// 点是否在标记内
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="marks"></param>
+        /// <returns></returns>
+        private static bool PointInMarker(Point point, params QRCodeMark[] marks)
+        {
+            foreach (QRCodeMark item in marks)
+            {
+                if (PointInSquare(point, item.LeftTopPoint, item.RightBottomPoint)) return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// 点是否在正方形内
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="leftTopPoint"></param>
+        /// <param name="rightBottomPoint"></param>
+        /// <returns></returns>
+        private static bool PointInSquare(Point point, Point leftTopPoint, Point rightBottomPoint)
+        {
+            if (point.X < leftTopPoint.X || point.X > rightBottomPoint.X) return false;
+            if (point.Y < leftTopPoint.Y || point.Y > rightBottomPoint.Y) return false;
+            return true;
+        }
+        private class QRCodeMark
+        {
+            public Point LeftTopPoint { get; set; }
+            public Point RightBottomPoint { get; set; }
         }
     }
 }
--- Materal/Test/Materal.Test/UtilsTests/BarCodeTests/QRCodeTest.cs
+++ Materal/Test/Materal.Test/UtilsTests/BarCodeTests/QRCodeTest.cs
@@ -1,5 +1,6 @@
 ﻿using Materal.Utils.BarCode;
 using SkiaSharp;
+using System.Drawing;
 
 namespace Materal.Test.UtilsTests.BarCodeTests
 {
@@ -9,71 +10,81 @@ namespace Materal.Test.UtilsTests.BarCodeTests
     [TestClass]
     public class QRCodeTest : MateralTestBase
     {
-        private string _input = string.Empty;
-        public QRCodeTest()
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                _input += "Poi!";
-            }
-        }
         /// <summary>
         /// 二维码测试
         /// </summary>
         [TestMethod]
-        public void QRCodeImageTest()
+        public void ChangeQRCodeTest()
         {
-            //创建二维码
-            string savePath = @"D:\QRCodeImage.png";
-            SKBitmap qrCodeImage = QRCodeHelper.CreateQRCode(_input, 900);
-            qrCodeImage.SaveAs(savePath);
-            //读取二维码
-            SKBitmap diskQRCodeImage = SKBitmap.Decode(savePath);
-            string result = QRCodeHelper.ReadQRCode(diskQRCodeImage);
-            Assert.AreEqual(_input, result);
-        }
-        /// <summary>
-        /// 彩色二维码测试
-        /// </summary>
-        [TestMethod]
-        public void QRCodeColorImageTest()
-        {
-            //创建二维码
-            string savePath = @"D:\QRCodeColorImage.png";
-            SKBitmap qrCodeImage = QRCodeHelper.CreateQRCode(_input, 900, SKColors.LightBlue, SKColors.White);
-            qrCodeImage.SaveAs(savePath);
-            //读取二维码
-            SKBitmap diskQRCodeImage = SKBitmap.Decode(savePath);
-            string result = QRCodeHelper.ReadQRCode(diskQRCodeImage);
-            Assert.AreEqual(_input, result);
-        }
-        /// <summary>
-        /// 自定义前景图片二维码测试
-        /// </summary>
-        [TestMethod]
-        public void QRCodeCustomForegroundImageTest()
-        {
-            //创建二维码
-            string savePath = @"D:\QRCodeCustomForegroundImage.png";
-            SKBitmap foregroundImage = SKBitmap.Decode(@"C:\Users\cloom\OneDrive\图片\表情包\PT头像.jpg");
-            SKBitmap qrCodeImage = QRCodeHelper.CreateQRCode(_input, foregroundImage, SKColors.Red);
-            qrCodeImage.SaveAs(savePath);
-            //读取二维码
-            SKBitmap diskQRCodeImage = SKBitmap.Decode(savePath);
-            string result = QRCodeHelper.ReadQRCode(diskQRCodeImage);
-            Assert.AreEqual(_input, result);
-        }
-        /// <summary>
-        /// 自定义背景图片二维码测试
-        /// </summary>
-        [TestMethod]
-        public void QRCodeCustomBackgroundImageTest()
-        {
-            //创建二维码
-            string savePath = @"D:\QRCodeCustomBackgroundImage.png";
-            SKBitmap backgroundImage = SKBitmap.Decode(@"C:\Users\cloom\OneDrive\图片\表情包\PT头像.jpg");
-            SKBitmap qrCodeImage = QRCodeHelper.CreateQRCode(_input, SKColors.Red, backgroundImage);
-            qrCodeImage.SaveAs(savePath);
+            string input = string.Empty;
+            for (int i = 0; i < 40; i++)
+            {
+                input += "Poi!";
+            }
+            int[] size = [
+                300,
+                600,
+                900
+            ];
+            foreach (int item in size)
+            {
+                //创建二维码
+                string savePath = @$"D:\Test\[{item}]QRCodeImage.png";
+                SKBitmap qrCodeImage = QRCodeHelper.CreateQRCode(input, item);
+                qrCodeImage.SaveAs(savePath);
+                SKColor GetPaintColor(Point centerPoint)
+                {
+                    if (centerPoint.Y > item / 3 * 2)
+                    {
+                        return SKColors.LightBlue;
+                    }
+                    else if (centerPoint.Y > item / 3)
+                    {
+                        return SKColors.Orange;
+                    }
+                    else
+                    {
+                        return SKColors.LightGreen;
+                    }
+                }
+                //创建圆点二维码
+                savePath = @$"D:\Test\[{item}]QRCodeImage-圆点.png";
+                SKBitmap newQrCodeImage = QRCodeHelper.ChangeQRCodeImage(qrCodeImage, (canvas, paint, centerPoint, pointSize) =>
+                {
+                    float radius = pointSize.Width / 2;
+                    paint.Color = GetPaintColor(centerPoint);
+                    canvas.DrawCircle(centerPoint.X, centerPoint.Y, radius, paint);
+                }, (paint, centerPoint) => paint.Color = GetPaintColor(centerPoint), SKColors.White);
+                newQrCodeImage.SaveAs(savePath);
+                //创建小方块二维码
+                savePath = @$"D:\Test\[{item}]QRCodeImage-方块.png";
+                newQrCodeImage = QRCodeHelper.ChangeQRCodeImage(qrCodeImage, (canvas, paint, centerPoint, pointSize) =>
+                {
+                    paint.Color = GetPaintColor(centerPoint);
+                    canvas.DrawRect(centerPoint.X - pointSize.Width / 2, centerPoint.Y - pointSize.Height / 2, pointSize.Width, pointSize.Height, paint);
+                }, (paint, centerPoint) => paint.Color = GetPaintColor(centerPoint), SKColors.White);
+                newQrCodeImage.SaveAs(savePath);
+                //创建图片二维码
+                savePath = @$"D:\Test\[{item}]QRCodeImage-图片.png";
+                string imagePath = @$"D:\Test\PT头像[{item}].png";
+                SKBitmap image = SKBitmap.Decode(imagePath);
+                newQrCodeImage = QRCodeHelper.ChangeQRCodeImage(qrCodeImage, (canvas, paint, centerPoint, pointSize) =>
+                {
+                    int startX = centerPoint.X - pointSize.Width / 2;
+                    int startY = centerPoint.Y - pointSize.Height / 2;
+                    for (int i = 0; i < pointSize.Width; i++)
+                    {
+                        for (int j = 0; j < pointSize.Height; j++)
+                        {
+                            int x = startX + i;
+                            int y = startY + j;
+                            paint.Color = image.GetPixel(x, y);
+                            canvas.DrawRect(x, y, 1, 1, paint);
+                        }
+                    }
+                }, (paint, centerPoint) => paint.Color = GetPaintColor(centerPoint), SKColors.White);
+                newQrCodeImage.SaveAs(savePath);
+            }
         }
     }
 }
